504eb9c4a53513f7cc1e72325a7fa866e237c7eb
曾繼昀CHEEYUNTSENG <cheeyuntseng@mitac.com.tw>
2024-07-11 15:51:39 +0800

    v1.4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,4 @@ export default defineNuxtConfig({
     LINE_CHANNEL_ACCESS_TOKEN: process.env.LINE_CHANNEL_ACCESS_TOKEN,
     LINE_CHANNEL_SECRET: process.env.LINE_CHANNEL_SECRET,
   },
-  nitro: {
-    devServer: {
-      port: process.env.PORT || 10000,
-      host: '0.0.0.0',
-    },
-  },
 });
