07379530d8af77a5f7b9ab0b60d357865b2eecb7
曾繼昀CHEEYUNTSENG <cheeyuntseng@mitac.com.tw>
2024-09-16 15:54:10 +0800

    更新正式url

--- server/api/createPayment.js
+++ server/api/createPayment.js
@@ -4,7 +4,7 @@ import crypto from 'crypto';
 export default defineEventHandler(async (event) => {
   const CHANNEL_ID = process.env.LINE_TEST_CHANNELID;
   const CHANNEL_SECRET = process.env.LINE_TEST_CHANNELSECRETKEY;
-  const BASE_URL = 'https://sandbox-api-pay.line.me';
+  const BASE_URL = 'https://api-pay.line.me';
   const URI = '/v3/payments/request';
 
   const body = await readBody(event);
